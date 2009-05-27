74db991b7f57966b8f605e7d3afb19eb49a2af30
Luís Guilherme Fernandes Pereira <lpereira@gmail.com>
2009-05-27 19:39:00 -0300

    Criei a estrutura do MILSP para gerar os dados do XPRESS

--- Programas/milsp.cc
+++ Programas/milsp.cc
@@ -1,8 +1,17 @@
 #include "milsp_instance.H"
+#include "integer_program.H"
 #include <cstdio>
 
-class MILSP {
+class MILSP : public IntegerProgram {
   MILSPInstance instance;
+  Instance& getInstance() { return instance; }
+  void getParam(int& ncol,int& nrow,char* rowtype,double* rhs,
+			double* obj,int* colbeg, int* rowidx,
+			double* matval,double* lb,double* ub,
+			int& nmip,char* miptype,int* mipcol,bool& relaxed) {
+    
+  }
+
 };
 
 int main(int argc, char* argv[]) {
