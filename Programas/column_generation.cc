8802e85615cf3ccb81391adc506c758a29bbfc64
Igor Assis <igor.assis@gmail.com>
2009-06-06 09:57:24 -0300

    Removidas linhas de DEBUG. Adicionados métodos getnlines() e getnrows() em IntegerProgram. Aloca variáveis duais.

--- Programas/column_generation.cc
+++ Programas/column_generation.cc
@@ -5,7 +5,7 @@
    escolhe as colunas
    while (1) {
    resolve o problema restrito (limitante primal do mestre relaxado)
-   verifica se é inteira (limitante primal do mestre)   
+   verifica se é inteira (limitante primal do mestre)
    resolve o problema de pricing (verifica se restrito é dual-viável)
    verifica se a solução do restrito é ótima (pricing)
      se não, gera colunas; continue
@@ -21,12 +21,12 @@ void ColumnGeneration::solveRestricted() {
   /* resolve o problema mestre reduzido                                       */
   /*========================================================================= */
   printf("\n==========================\nOtimizacao do PMR \n");
-	    
+
   /* inicializa o pool de colunas */
   sol.totPool=0;
 
   XPRSinitglobal(probMestre); /* reinicializa o prob mestre reduzido*/
-	    
+
   /* resolve o PMR com o optimizer */
   t1=clock();
   xpress_ret=XPRSminim(probMestre,NULL); /* g = algoritmo de busca B&B, NULL = PL */
@@ -35,7 +35,7 @@ void ColumnGeneration::solveRestricted() {
   tempo=((double)(t2-t1))/CLOCKS_PER_SEC;
   tempoLP+=t2-t1;
 
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro na chamada da rotina XPRSminim.\n",__LINE__,xpress_ret, probMestre);
 
   if (isIntegerSol(probMestre)){ /* obtem valor das var duais e valor da solucao */
@@ -57,26 +57,26 @@ void ColumnGeneration::solvePricing(Instance& instance) {
   /* resolve o problema de pricing                                            */
   /*========================================================================= */
   printf("\n==========================\nOtimizacao do Pricing \n");
-  
+
   /* calcula os novos coeficientes das variaveis na funcao objetivo do subproblema de pricing */
   double* obj_pricing = instance.getNewObj(dual,mindex);
-    
+
   /* resolve o sub de pricing */
   /* uma vez maximizado, perde a base inicial, e o problema tem que ser carregado de novo? */
   XPRSinitglobal(probPricing); /* reinicializa o prob de pricing */
-  
+
   XPRSchgobj(probPricing,instance.dim(),mindex,obj_pricing); /* seta novos coeficientes na funcao objetivo */
   /* reinicializa a melhor solucao */
-  sol.zstar=XPRS_MINUSINFINITY; 
+  sol.zstar=XPRS_MINUSINFINITY;
   xpress_ret=XPRSsetdblcontrol(probPricing,XPRS_MIPABSCUTOFF,sol.zstar);
-  
+
   t1=clock();
   xpress_ret=XPRSmaxim(probPricing,"g"); /* g = algoritmo de busca B&B, NULL = PL */
   t2=clock();
-  
+
   tempo=((double)(t2-t1))/CLOCKS_PER_SEC;
   tempoPricing+=t2-t1;
-  
+
   /* imprime resultado do pricing */
   printf("\n==========================\nResultado do Pricing");
   printf("\nZstar (custo reduzido): %lf", 1-sol.zstar);
@@ -88,8 +88,8 @@ ColumnGeneration::ColumnGeneration() {
   /* inicializacão do XPRESS                   */
   /* ========================================= */
   xpress_ret=XPRSinit("");
-  if (xpress_ret) 
-    errormsg("Main: Erro de inicializacao do XPRESS.\n",__LINE__,xpress_ret, probMestre);
+  if (xpress_ret)
+    errormsg("Main: Erro de inicializacao do XPRESS.\n", __LINE__, xpress_ret, probMestre);
 
   /* inicializa valores de variaveis globais */
   tPMR=0;
@@ -100,92 +100,91 @@ ColumnGeneration::ColumnGeneration() {
 /* Cria, Carrega e configura cada modelo */
 void ColumnGeneration::configureModel(int formato, IntegerProgram& ip, IntegerProgram& pricing) {
   int xpress_ret;
-  
+
   /* "cria" o problema  mestre reduzido */
   xpress_ret=XPRScreateprob(&probMestre);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro na initializacao do problema",__LINE__,xpress_ret, probPricing);
-  
+
   /* "cria" o problema Pricing */
   xpress_ret=XPRScreateprob(&probPricing);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro na initializacao do problema",__LINE__,xpress_ret, probPricing);
-  
+
   /* ==================================================================================== */
   /* Carga e configuracao dos parametros de controle do XPRESS para o subprob de pricing  */
   /* ==================================================================================== */
-  
 
   /* ALTERAR DEPOIS!!!  */
   /* limita o tempo de execucao */
   // xpress_ret=XPRSsetintcontrol(probPricing,XPRS_MAXTIME,MAX_CPU_TIME);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro ao tentar setar o XPRS_MAXTIME.\n",__LINE__,xpress_ret, probPricing);
-  
+
   // /* carga do modelo de pricing */
   // xpress_ret=loadModel(pricing,probPricing,"Pricing");
   // if (xpress_ret) errormsg("Main: Erro na carga do modelo.",__LINE__,xpress_ret, probPricing);
-  
+
   // /* salva um arquivo ".lp" com o LP original */
   // xpress_ret=XPRSwriteprob(probPricing,"Pricing","l");
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro na chamada da rotina XPRSwriteprob.\n",__LINE__,xpress_ret, probPricing);
-  
+
   // /* Desabilita o PRESOLVE */
   // xpress_ret=XPRSsetintcontrol(probPricing,XPRS_PRESOLVE,0);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro ao desabilitar o presolve.",__LINE__,xpress_ret, probPricing);
-  
+
   // /* impressão para conferência */
   // xpress_ret=XPRSsetintcontrol(probPricing,XPRS_MIPLOG,formato);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro ao setar MIPLOG.",__LINE__,xpress_ret, probPricing);
-  
+
   // /* Desabilita heuristica */
   // xpress_ret=XPRSsetintcontrol(probPricing,XPRS_HEURSTRATEGY,0);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro ao tentar setar o XPRS_CUTSTRATEGY.\n",__LINE__,xpress_ret, probPricing);
-  
+
   // /* Desabilita a separacao de cortes do XPRESS.  */
   // xpress_ret=XPRSsetintcontrol(probPricing,XPRS_CUTSTRATEGY,0);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro ao tentar setar o XPRS_CUTSTRATEGY.\n",__LINE__,xpress_ret, probPricing);
-  
+
   // /* callback para salvar a melhor solucao inteira encontrada */
   // xpress_ret=XPRSsetcbintsol(probPricing,salvaMelhorSol, &sol);
-  // if (xpress_ret) 
+  // if (xpress_ret)
   //   errormsg("Main: Erro na chamada da rotina XPRSsetcbintsol\
   // 			.\n",__LINE__,xpress_ret, probPricing);
-  
+
   /* ===================================================================================== */
   /* carga e Configuracao dos parametros de controle do XPRESS para o problema mestre red  */
   /* ===================================================================================== */
-  
+
   /* limita a qtde de colunas a serem incluidas */
   XPRSsetintcontrol(probMestre,XPRS_EXTRACOLS,50);
-  
+
   /* limita o tempo de execucao */
   xpress_ret=XPRSsetintcontrol(probMestre,XPRS_MAXTIME,MAX_CPU_TIME);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro ao tentar setar o XPRS_MAXTIME.\n",__LINE__,xpress_ret, probMestre);
-  
+
   /* carga do problema mestre */
   xpress_ret=loadModel(ip, probMestre, "Mestre");
   if (xpress_ret) errormsg("Main: Erro na carga do modelo.",__LINE__,xpress_ret, probMestre);
-  
+
+  /* aloca variaveis duais */
+  dual = (double *)calloc(ip.getnrows(), sizeof(double));
+
   /* Desabilita o PRESOLVE */
   xpress_ret=XPRSsetintcontrol(probMestre,XPRS_PRESOLVE,0);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro ao desabilitar o presolve.",__LINE__,xpress_ret, probMestre);
 
   /* salva um arquivo ".lp" com o LP original */
   xpress_ret=XPRSwriteprob(probMestre,"Mestre","l");
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("Main: Erro na chamada da rotina XPRSwriteprob.\n",__LINE__,xpress_ret, probMestre);
 }
-
-
-
 
 /* =====================================================================
  * Rotina (callback) para salvar a  melhor solucao.  Roda para todo nó
@@ -205,30 +204,30 @@ void XPRS_CC salvaMelhorSol(XPRSprob prob, void *psol)
 
   /* pega o numero do nó corrente */
   xpress_ret=XPRSgetintattrib(prob,XPRS_NODES,&node);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("salvaMelhorSol: rotina XPRSgetintattrib.\n",__LINE__,xpress_ret, prob);
 
   /* recupera numero de colunas do prob */
   xpress_ret=XPRSgetintattrib(prob,XPRS_COLS,&cols);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("salvaMelhorSol: rotina XPRSgetintattrib\n",__LINE__,xpress_ret, prob);
 
   /* recupera solucao */
   x=(double *)malloc(cols*sizeof(double));
 
   xpress_ret=XPRSgetdblattrib(prob,XPRS_LPOBJVAL,&objval);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("salvaMelhorSol: rotina XPRSgetdblattrib\n",__LINE__,xpress_ret, prob);
 
   xpress_ret=XPRSgetsol(prob,x,NULL,NULL,NULL);
-  if (xpress_ret) 
+  if (xpress_ret)
     errormsg("salvaMelhorSol: Erro na chamada da rotina XPRSgetsol\
 			\n",__LINE__,xpress_ret, prob);
 
   /* guarda no pool de colunas se o custo reduzido da solucao inteira é negativo */
   if (1-objval<0 && sol->totPool<MAX_POOL){
     (sol->pool)[sol->totPool] = std::vector<double>(cols);
-    for(i=0;i<cols;i++) 
+    for(i=0;i<cols;i++)
       (sol->pool)[sol->totPool][i]=x[i];
     (sol->totPool)++;
   }
@@ -237,34 +236,34 @@ void XPRS_CC salvaMelhorSol(XPRSprob prob, void *psol)
   viavel = true;
 
   /* se a solucao tiver custo melhor que a melhor solucao disponivel entao salva */
-  if ((objval > sol->zstar) && viavel ) { 
-    for(i=0;i<cols;i++) 
+  if ((objval > sol->zstar) && viavel ) {
+    for(i=0;i<cols;i++)
       sol->xstar[i]=x[i];
     sol->zstar=objval;
-     
+
     /* informa xpress sobre novo incumbent */
     xpress_ret=XPRSsetdblcontrol(prob,XPRS_MIPABSCUTOFF,sol->zstar);//zstar+1.0-EPSILON);
-    if (xpress_ret) 
+    if (xpress_ret)
       errormsg("salvaMelhorSol: XPRSsetdblcontrol.\n",__LINE__,xpress_ret, prob);
-  }   
+  }
   free(x);
   return;
 }
 
 /* ========================================= */
-/* IsIntegerSol                             */
+/* IsIntegerSol                              */
 /* ========================================= */
 /* obtem o valor das variaveis duais e o valor da solucao. E retorna se sol eh inteira */
 inline bool ColumnGeneration::isIntegerSol(XPRSprob prob) {
   int i, inteiro, solfile, colunas;
   double *lambda, *obj;
-	      
+
   /* recupera dados do problema */
   XPRSgetintattrib(prob,XPRS_COLS,&colunas); /* recupera numero de colunas  */
 
   lambda = (double *)malloc(colunas*sizeof(double));
   obj=(double *)malloc(colunas*sizeof(double));
-	
+
   /* recupera dados do problema */
   XPRSgetobj(prob,obj, 0, colunas-1); /* recupera coef das var na f.o.*/
 
@@ -273,7 +272,7 @@ inline bool ColumnGeneration::isIntegerSol(XPRSprob prob) {
   XPRSsetintcontrol(prob,XPRS_SOLUTIONFILE,0);		/* reseta */
   XPRSgetsol(prob,lambda, NULL, dual, NULL);
   XPRSsetintcontrol(prob,XPRS_SOLUTIONFILE,solfile);		/* volta config anterior */
-	      
+
   /* testar se solucao lambda eh inteira. Se for, ver se solucao eh melhor e guardar  */
   inteiro=1;
   z_PMR=0.0;
@@ -287,32 +286,32 @@ inline bool ColumnGeneration::isIntegerSol(XPRSprob prob) {
   return inteiro;
 }
 
-
 /* Esta é a função mais importante: é ela que transforma a instância
    em um problema do XPRESS. A idéia é fazê-la o mais genérica
    possível e transferir responsabilidade para métodos da
    instância. Com isso, poderemos resolver qualquer problema, desde
    que ele saiba se transformar em uma entrada do XPRESS (via método
-   getParam) 
+   getParam)
 
    Mais importante, podemos usar o mesmo método para carregar o
    problema mestre e o problema de pricing. Os antigos
    CargaProblemaPricing e CargaProblemaMestre serão transformados em
-   loadModel  
+   loadModel
 */
 int ColumnGeneration::loadModel(IntegerProgram& ip,XPRSprob prob,std::string name) {
-  int ncol, nrow,nmip; 
+  int ncol, nrow,nmip;
   char* rowtype,* miptype;
   double* rhs,* obj,* lb,* ub,* matval;
   int* colbeg,* rowidx,*mipcol;
   bool relaxed;
+
   ip.getParam(ncol,nrow,&rowtype,&rhs,&obj,&colbeg,&rowidx,&matval,&lb,
 	      &ub,nmip,&miptype,&mipcol,relaxed);
+
   if (relaxed) {
     xpress_ret = XPRSloadlp(prob,name.c_str(),ncol,nrow,rowtype,rhs,
 			    NULL,obj,colbeg,NULL,rowidx,matval,lb,ub);
-  }
-  else { // integer 
+  } else { // integer
     xpress_ret = XPRSloadglobal(prob,name.c_str(),ncol,nrow,rowtype,rhs,
 				NULL,obj,colbeg,NULL,rowidx,matval,lb,
 				ub,nmip,0,miptype,mipcol,NULL,NULL,NULL,
@@ -325,12 +324,12 @@ int ColumnGeneration::loadModel(IntegerProgram& ip,XPRSprob prob,std::string nam
  * Rotina copiada de programas exemplo do XPRES ... :(
  * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  *
- * Name:         errormsg 
+ * Name:         errormsg
  * Purpose:      Display error information about failed subroutines.
- * Arguments:    const char *sSubName   Subroutine name             
- *               int nLineNo            Line number                 
- *               int nErrCode           Error code                  
- * Return Value: None                                               
+ * Arguments:    const char *sSubName   Subroutine name
+ *               int nLineNo            Line number
+ *               int nErrCode           Error code
+ * Return Value: None
 \**********************************************************************************/
 void errormsg(const char *sSubName,int nLineNo,int nErrCode, XPRSprob probMestre)
 {
--- Programas/milsp.cc
+++ Programas/milsp.cc
@@ -12,20 +12,28 @@
 class MILSP : public IntegerProgram {
   std::vector<std::vector<double> > columns; //colunas transpostas;
   std::vector<double> cost; //tamanho dinâmico
-  int ncols;
+  int ncols, nrows;
   MILSPInstance instance;
 public:
-  Instance& getInstance() { return instance; }
+
+  int getncols() {
+    return ncols;
+  }
+
+  int getnrows() {
+    return nrows;
+  }
+
   void getParam(int& ncol,int& nrow,char** rowtype,double** rhs,
 		double** obj,int** colbeg, int** rowidx,
 		double** matval,double** lb,double** ub,
 		int& nmip,char** miptype,int** mipcol,bool& relaxed) {
 
     relaxed = true;
-    
+
     /* Aloca memória dinamicamente para o XPress */
     ncol = ncols;
-    nrow = 2*instance.t + instance.m;
+    nrow = nrows;
     nmip = 0; miptype = NULL; mipcol = NULL;
     *rowtype = (char *) calloc(nrow,sizeof(char));
     *rhs = (double *) calloc(nrow,sizeof(double));
@@ -45,17 +53,17 @@ public:
       (*rowtype)[pos] = 'L';
       (*rhs)[pos] = instance.C[i];
     }
-    
+
     for(int pos = 2*instance.t, i = 0;i<instance.m;pos++,i++) {
       (*rowtype)[pos] = 'E';
       (*rhs)[pos] = 1;
     }
-    
+
     int matsize = 0;
-    for(int col=0;col<ncols;col++) 
-      for(int row=0;row<sz(columns[col]);row++) 
-	matsize += NONZERO(columns[col][row]); 
-    
+    for(int col=0;col<ncols;col++)
+      for(int row=0;row<sz(columns[col]);row++)
+	matsize += NONZERO(columns[col][row]);
+
     /* aloca matriz esparsa */
     *matval = (double *) calloc(matsize,sizeof(double));
     *rowidx = (int *) calloc(matsize,sizeof(int));
@@ -73,6 +81,7 @@ public:
       }
     }
     (*colbeg)[ncols] = nnzero;
+
 #ifdef PRINTDEBUG
     for(int i=0;i<nrow;i++) {
       for(int j=0;j<ncol;j++) {
@@ -80,24 +89,27 @@ public:
       }
       printf("%.1lf\n",(*rhs)[i]);
     }
-#endif 
+#endif
+
   }
 
   /* TODO: Gerar planos de produção que sempre dão uma solução viável.
-           + Produzir sempre a capacidade maxima para todo item 
+           + Produzir sempre a capacidade maxima para todo item
 	     em todo período
 	   + Com esses planos é sempre possível obter uma solução
 	     da Relaxação do Meste Restrito
-   */  
+   */
 
   MILSP(MILSPInstance& instance) {
     this->instance = instance;
-    
+
+    nrows = 2*instance.t + instance.m;
     ncols = instance.m*instance.t;
+
     vvi x(ncols);
     vvi y(ncols);
     vvi s(ncols);
-    
+
     vi demanda;
     for(int prod=0;prod<instance.m;prod++) {
       demanda.pb(0);
@@ -110,7 +122,7 @@ public:
       for(int tp=0;tp<instance.t;tp++,sch++) {
 	int dem = demanda[prod];
 	for(int inst=0;inst<instance.t;inst++) {
-	  if (inst < tp) { 
+	  if (inst < tp) {
 	    int toprod = instance.d[prod][inst];
 	    dem -= toprod;
 	    x[sch].pb(toprod);
@@ -127,12 +139,12 @@ public:
 	    x[sch].pb(0);
 	    y[sch].pb(0);
 	    dem -= instance.d[prod][inst];
-	    s[sch].pb(dem);		    
+	    s[sch].pb(dem);
 	  } // end else
 	} // end for inst
       } // end for tp
     } // end for prod
-      
+
     columns = std::vector<std::vector<double> >(ncols);
     for(int col=0;col<ncols;col++) {
       columns[col] = std::vector<double>(2*instance.t + instance.m);
@@ -144,10 +156,8 @@ public:
     cost = std::vector<double>(ncols);
     /* Gera as colunas baseado no x */
     for(int prod=0,col=0;prod<instance.m;prod++) {
-      std::cerr << prod << std::endl;
       for(int tp=0;tp<instance.t;tp++,col++) {
 	int row = 0;
-	std::cerr << "-- " << tp << std::endl;
 	// columns[col] = std::vector<double>(2*instance.t + instance.m);
 	for(int inst=0;inst<instance.t;inst++,row++) {
 	  columns[col][row] = y[col][inst];
@@ -158,7 +168,7 @@ public:
 	for(int k=0;k<instance.m;k++,row++) {
 	  columns[col][row] = ((k == prod)?1:0);
 	}
-	
+
 	cost[col] = 0;
 	for(int inst=0;inst<instance.t;inst++) {
 	  cost[col] += instance.p[prod][inst]*x[col][inst] +
@@ -179,12 +189,12 @@ public:
       columns[ncols++][i] = 1.0;
       cost.push_back(inf);
     }
-      
+
   } // end MILSP
- 
+
   void OldMILSP(MILSPInstance& instance) {
     this->instance = instance;
-    
+
     ncols = instance.m;
     vvi x(ncols);
 
@@ -193,7 +203,7 @@ public:
 	x[sch].pb(instance.d[sch][inst]);
       }
     }
-    
+
     /* gera as primeiras colunas */
     columns = std::vector<std::vector<double> >(ncols);
     for(int col=0;col<ncols;col++) {
@@ -212,15 +222,15 @@ public:
 	columns[col].pb(val);
       }
 
-      /* custo do esquema */ 
+      /* custo do esquema */
       cost.pb(0);
       for(int inst=0;inst<instance.t;inst++) {
 	cost[col] += instance.p[col][inst]*x[col][inst] + instance.f[col][inst];
       }
-      
+
     } // fim for col
   }
-    
+
 };
 
 
